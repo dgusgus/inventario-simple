71f4bfb68a650fa5229b65de34567d1d0da9d9da
dgusgus <dgusgus1@gmail.com>
2026-08-24 22:15:38 -0400

    despliegue del sistema

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/inventario-simple/", // debe coincidir EXACTAMENTE con el nombre del repo, mayúsculas incluidas
   plugins: [vue(), tailwindcss()],
 });
